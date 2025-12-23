a0dfb9a02fe97d40fd8140a700b126961a18921e
V3nusAsABoy <andrewalloniealt@gmail.com>
2025-12-23 11:11:42 -0500

    changed about page to add ee

--- react/src/components/home.tsx
+++ react/src/components/home.tsx
@@ -1,7 +1,10 @@
+import eye from '../img/eye.png';
+
 export default function home(){
     return(
     <div id="home">
          <h1>Welcome to The Art Zone! Explore different pieces by our wonderful artists!</h1>
+         <img src={eye} style={{width:'20em'}}/>
     </div>
     );
 }
